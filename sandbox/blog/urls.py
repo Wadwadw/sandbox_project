07b0647413016ac02832a717ed25fea5a1c01fc3
Vadym <sumsunh777@gmail.com>
2022-01-12 14:17:38 +0200

    Added article list functionality.

--- sandbox/blog/urls.py
+++ sandbox/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleList
+from .views import ArticleListView, ArticleDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('article-list/', ArticleList.as_view())
+    path('article-list/', ArticleListView.as_view()),
+    path('article-detail/<int:pk>', ArticleDetailView.as_view())
 ]
--- sandbox/blog/views.py
+++ sandbox/blog/views.py
@@ -3,19 +3,48 @@ from rest_framework.response import Response
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework import status
+from django.http import Http404
 
 
-class ArticleList(APIView):
+class ArticleListView(APIView):
 
-    def get(self, request, format=None):
+    @staticmethod
+    def get(request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
+    @staticmethod
+    def post(request):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ArticleDetailView(APIView):
+    @staticmethod
+    def get_object(pk):
+        try:
+            return Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
